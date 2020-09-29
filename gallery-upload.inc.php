3b3d59821f19ba048df1b88000f43229ecac747e
hrithik109 <70449792+hrithik109@users.noreply.github.com>
2020-09-29 16:12:46 +0530

    Add files via upload

--- gallery-upload.inc.php
+++ gallery-upload.inc.php
@@ -1,73 +1,78 @@
 <?php
-
-if (isset($_POST['submit'])){
+if(isset($_POST['submit'])){
     $newFileName=$_POST['filename'];
-    if (empty($newFileName)){
-        $newFileName = "gallery";
+    if(empty($newFileName)){
+        $newFileName="gallery";
     }
     else{
-        $newFileName = strtolower(str_replace(" ","-",$newFileName));
+        $newFileName=strtolower(str_replace("","-",$newFileName));
     }
-    $imageTitle = $_POST['filetitle'];
-    $imageDesc = $_POST['filedesc'];
+$imageTitle=$_POST['filetitle'];
+$imageDesc=$_POST['filedesc'];
 
-    $file = $_FILES['file'];
+$file=$_FILES['file'];
 
-    $fileName = $file["name"];
-    $fileType = $file["type"];
-    $fileTempName = $file["tmp_name"];
-    $fileError = $file["error"];
-    $fileSize = $file["size"];
+$fileName=$file["name"];
+$fileType=$file["type"];
+$fileTempName=$file["tmp_name"];
+$fileError=$file["error"];
+$fileSize=$file["size"];
 
-    $fileExt = explode(".", $fileName);
-    $fileActualExt =strtolower(end($fileExt));
+$fileExt=explode(".",$fileName);
+$fileActualExt=strtolower(end($fileExt));
 
-    $allowed = array("jpg","jpeg","png");
+$allowed=array("jpg","jpeg","png","pdf");
 
-    if (in_array($fileActualExt,$allowed)){
-        if ($fileError == 0){
-            if($fileSize < 2000000){
-                $imageFullName = $newFileName . "." . uniqid("",true) . "." . $fileActualExt;
-                $fileDestination = "C:/" . $imageFullName;
-                
-                include_once "dbh.inc.php";
+if(in_array($fileActualExt,$allowed)){
+   if($fileError===0){
+      if($fileSize<2000000){
 
-                if (empty($imageTitle) || empty($imageDesc)){
-                    header("Location: welcome.php?upload=empty");
-                    exit();
-                }
-                else {
-                    $sql = "SELECT * FROM  gallery";
-                    $stmt = mysqli_stmt_init($conn);
-                    if (!mysqli_stmt_prepare($stmt,$sql)){
-                        echo " 1 SQL statement failed!";
-                    }
-                    else{
-                        mysqli_stmt_execute($stmt);
-                        $result = mysqli_stmt_get_result($stmt);
-                        $rowCount = mysqli_num_rows($result);
-                        $setImageOrder = $rowCount + 1;
+           $imageFullName=$newFileName.".".uniqid("",true).".".$fileActualExt;
+           $fileDestination="../img/gallery/".$imageFullName;
 
-                        $sql = "INSERT INTO gallery (titleGallery,descGallery,imgFullNameGallery,orderGallery) VALUES (?,?,?,?)";
-                        if(!mysqli_stmt_prepare($stmt,$sql)){
-                            echo "2 SQL statement failed!";
-                        }
-                        else{
-                            mysqli_stmt_bind_param($stmt, "sss", $imageDesc, $imageFullName,$setImageOrder);
-                            mysqli_stmt_execute($stmt);
+           include_once "gallerydbh.inc.php";
 
-                            move_uploaded_file($fileTempName, $fileDestination);
-                            header("Location: welcome.php?upload=success");
-                        }
-                            
-                    }
-                }
-            }
-            else{
-                echo "File size is too big!";
-                exit();
-            }
-        }
-    }
+           if(empty($imageTitle)||empty($imageDesc)){
+               header("Location:../gallery.php?upload=empty");
+               exit();
+           }else{
+              $sql="SELECT * FROM gallery;";
+              $stmt=mysqli_stmt_init($conn);
+
+              if(!mysqli_stmt_prepare($stmt,$sql)){
+                  echo "sql statement failed";
+              }else{
+                  mysqli_stmt_execute($stmt);
+                  $result=mysqli_stmt_get_result($stmt);
+                  $rowCount=mysqli_num_rows($result);
+                  $setImageOrder=$rowCount+1;
+
+
+                  $sql="INSERT INTO gallery(titleGallery,descGallery,imgFullNameGallery,orderGallery) VALUE(?,?,?,?);";
+                    
+                 if(!mysqli_stmt_prepare($stmt,$sql)){
+                      echo "sql statement error";
+                  }else{
+                      mysqli_stmt_bind_param($stmt,"ssss",$imageTitle,$imageDesc,$imageFullName,$setImageOrder);
+                      mysqli_stmt_execute($stmt);
+
+                      move_uploaded_file($fileTempName,$fileDestination);
+                      
+                      header("Location:../gallery.php?uploadd=success");
+                  }
+              }
+           }
+
+      }else{
+          echo "file size is too big";
+      }
+   }
+   else{
+       echo "you had an error";
+   }
 }
-?>
+else{
+    echo "you need to upload a proper file type!";
+    exit();
+}
+}
